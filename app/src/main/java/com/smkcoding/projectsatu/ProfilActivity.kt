105ba517452fb068b890256ce95dfd6834dccf88
febrio_d <7g26febrio@gmail.com>
2020-05-04 21:54:51 +0700

    project satu

--- app/src/main/java/com/smkcoding/projectsatu/ProfilActivity.kt
+++ app/src/main/java/com/smkcoding/projectsatu/ProfilActivity.kt
@@ -2,6 +2,7 @@ package com.smkcoding.projectsatu
 
 import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -13,23 +14,6 @@ class ProfilActivity : AppCompatActivity() {
         val REQUEST_CODE = 100
     }
 
-    private fun navigasiKeEditNama(){
-        val intent = Intent(this, EditNameActivity::class.java)
-        val namaUser = txtName.text.toString()
-        intent.putExtra("nama", namaUser)
-        startActivityForResult(intent, REQUEST_CODE)
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (requestCode == REQUEST_CODE)
-            if(resultCode == Activity.RESULT_OK) {
-                val result = data?.getStringExtra("nama")
-                txtName.text = result
-            }else{
-                Toast.makeText(this, "Edit Failed", Toast.LENGTH_SHORT).show()
-            }
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profil)
@@ -37,6 +21,10 @@ class ProfilActivity : AppCompatActivity() {
         ambilData()
 
         btnEditName.setOnClickListener { navigasiKeEditNama() }
+
+        btnDial.setOnClickListener { dialPhoneNumber(txtTelp.text.toString()) }
+
+        btnAbout.setOnClickListener { goToAboutActivity() }
 
     }
 
@@ -58,5 +46,35 @@ class ProfilActivity : AppCompatActivity() {
         txtAddress.text=alamat
 
     }
-}
 
+    private fun navigasiKeEditNama(){
+        val intent = Intent(this, EditNameActivity::class.java)
+        val namaUser = txtName.text.toString()
+        intent.putExtra("nama", namaUser)
+        startActivityForResult(intent, REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_CODE)
+            if(resultCode == Activity.RESULT_OK) {
+                val result = data?.getStringExtra("nama")
+                txtName.text = result
+            }else{
+                Toast.makeText(this, "Edit Failed", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun dialPhoneNumber(phoneNumber: String) {
+        val intent = Intent(Intent.ACTION_DIAL).apply {
+            data = Uri.parse("tel:$phoneNumber")
+        }
+        if (intent.resolveActivity(packageManager) !=null){
+            startActivity(intent)
+        }
+    }
+
+    private fun goToAboutActivity(){
+        val intent = Intent(this, AboutActivity::class.java)
+        startActivity(intent)
+    }
+}
